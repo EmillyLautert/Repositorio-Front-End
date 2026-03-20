227ffa738812db7f4850da86082818faf0e05e41
Emilly Lautert dos Santos <emilly.santos@atitus.local>
2026-03-20 20:46:38 -0300

    Exercícios iniciais de JavaScript

---
+++ Aula 20.03/script.js
@@ -0,0 +1,19 @@
+// Exercício 1
+// Criar uma variável chamada nome com o valor Emilly
+let nome = "Emilly";
+// Exibir o valor de nome no console
+console.log(nome);
+
+// Exercício 2
+// Criar duas variáveis chamadas preço e quantidade com valor float e int respectivamente
+let preco = 49.99;
+let quantidade = 3;
+// Exibir valores de preço e quantidade no console
+console.log(preco)
+console.log(quantidade)
+
+// Exercício 3
+// Criar variável chamada valor total com o valor sendo a multiplicação do preço pela quantidade
+let valorTotal = preco*quantidade
+// Exibir valor total
+console.log(valorTotal)
