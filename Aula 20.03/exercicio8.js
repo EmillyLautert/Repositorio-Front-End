8b532e5b989ebbed8368d29a6b33439292eef555
Emilly Lautert dos Santos <emilly.santos@atitus.edu.br>
2026-03-20 21:25:28 -0300

    Exercício 8

--- Aula 20.03/exercicio8.js
+++ Aula 20.03/exercicio8.js
@@ -4,5 +4,6 @@ let nomeProduto = "Celular"
 let valor = 6000
 let quantidade1 = 3
 let totalCompra = valor*quantidade1
+let saida = nomeProduto + ", valor unitário: " + valor + ", total da compra: " + totalCompra
 //Exibir valores com frase no console
-console.log(nomeProduto + ", valor unitário: " + valor + ", total da compra: " + totalCompra)
+console.log(saida)
