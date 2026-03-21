c44e36b296e907b37a500ba7626b0f3c0d1df92c
Emilly Lautert dos Santos <emilly.santos@atitus.edu.br>
2026-03-20 21:57:37 -0300

    exercício 9

--- Aula 20.03/exercicio9.js
+++ Aula 20.03/exercicio9.js
@@ -1,5 +1,9 @@
 // Exercício 9
-// Crie 3 variáveis: nomeProduto, valor e quantidade
-function nomeFuncao() {
-// código da função
+// Crie um programa para exibir o resultado de três produtos
+function geraMensagem(nomeProduto, valor, quantidade) {
+    console.log(`${nomeProduto}, valor unitário: ${valor}, total da compra: ${quantidade*valor}`);
 }
+
+geraMensagem("Iphone", "6000", "3");
+geraMensagem("xBox", "4500", "2");
+geraMensagem("Notebook", "8000", "4");
