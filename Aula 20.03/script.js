d0da79682fd22af702a806208c451d2d8bffcfb5
Emilly Lautert dos Santos <emilly.santos@atitus.local>
2026-03-20 21:05:02 -0300

    Continuação dos exercícios JavaScript

--- Aula 20.03/script.js
+++ Aula 20.03/script.js
@@ -1,19 +1,51 @@
 // Exercício 1
 // Criar uma variável chamada nome com o valor Emilly
-let nome = "Emilly";
+let nome1 = "Emilly";
 // Exibir o valor de nome no console
-console.log(nome);
+console.log(nome1);
 
 // Exercício 2
 // Criar duas variáveis chamadas preço e quantidade com valor float e int respectivamente
 let preco = 49.99;
 let quantidade = 3;
 // Exibir valores de preço e quantidade no console
-console.log(preco)
-console.log(quantidade)
+console.log(preco);
+console.log(quantidade);
 
 // Exercício 3
 // Criar variável chamada valor total com o valor sendo a multiplicação do preço pela quantidade
-let valorTotal = preco*quantidade
+let valorTotal = preco*quantidade;
 // Exibir valor total
-console.log(valorTotal)
+console.log(valorTotal);
+
+// Exercício 4
+// Criar duas variáveis chamadas nome e idade com valor str e int respectivamente
+let nome = "Emilly";
+let idade = 26;
+// Exibir valores de nome e idade no console com frase
+console.log("Olá, me chamo " + nome + " e minha idade é " + idade + " anos.");
+
+//Exercício 5
+// Teste do que será exibido no console
+let valorProduto;
+console.log(valorProduto)
+
+// Exercício 6
+// Teste do que será exibido no console
+let valorProduto1 = null;
+console.log(valorProduto1);
+
+// Exercício 7
+// Teste do que será exibido no console
+/*let valorProduto2 = “6000”;
+let imposto = “750”;
+console.log(valorProduto2 + imposto);*/
+
+// Exercício 8
+// Crie 3 variáveis: nomeProduto, valor e quantidade
+let nomeProduto = "Celular"
+let valor = 6000
+let quantidade1 = 3
+let totalCompra = valor*quantidade1
+//Exibir valores com frase no console
+console.log(nomeProduto + ", valor unitário: " + valor + ", total da compra: " + totalCompra)
