e6b5e7b456395014d8455e8774fdb0268b514e60
Emilly Lautert dos Santos <emilly.santos@atitus.edu.br>
2026-03-20 21:32:13 -0300

    ajuste exercícios

--- Aula 20.03/exercicio4.js
+++ Aula 20.03/exercicio4.js
@@ -3,4 +3,7 @@
 let nome = "Emilly";
 let idade = 26;
 // Exibir valores de nome e idade no console com frase
+// contatenação de str
 console.log("Olá, me chamo " + nome + " e minha idade é " + idade + " anos.");
+// template literals (crase)
+console.log(`Olá, me chamo ${nome} e minha idade é ${idade} anos.`)
