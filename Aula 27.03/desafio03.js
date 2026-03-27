7ab044065a0571df100104efc8100847aaf90bea
Emilly Lautert dos Santos <emilly.santos@atitus.edu.br>
2026-03-27 20:43:19 -0300

    ajuste no desafio 03

--- Aula 27.03/desafio03.js
+++ Aula 27.03/desafio03.js
@@ -1,10 +1,11 @@
 const numeros = [1, 2, 2, 3, 3, 4, 5, 5];
-const unicos = [];
+const unicos = []; // armazena um de cada vez
 
 for (let i = 0; i < numeros.length; i++) {
-  if (!unicos.includes(numeros[i])) {
-    unicos.push(numeros[i]);
+  if (!unicos.includes(numeros[i])) { //includes informa se já existe, a exclamação inverte para não existe
+    unicos.push(numeros[i]); //adiciona se não existe ainda
   }
 }
 
+console.log(unicos);
 console.log(unicos.length);
